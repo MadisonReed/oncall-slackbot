f075a1b988867ac15fcc4a3302456a626d60e44e
Jk Jensen <jk@mystenlabs.com>
2023-11-08 09:55:01 -0700

    remove unnecessary build script, add npm bump

--- build-and-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-version=$(cat package.json | jq -r '.version')
-echo "building and pushing version $version"
-docker build -t iamjk/oncall-slackbot:$version ./ -f Dockerfile
-docker push iamjk/oncall-slackbot:$version
