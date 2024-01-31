081f98e789c4d9335592bafe6de11b4df831609a
Jk Jensen <jk@mystenlabs.com>
2024-01-31 10:39:09 -0700

    rearrange listeners

--- src/listeners/index.ts
+++ src/listeners/index.ts
@@ -3,10 +3,10 @@ import events from './events/index';
 import messages from './messages/index';
 
 const registerListeners = (app: App) => {
-  // actions.register(app);
-  // commands.register(app);
   events.register(app);
   messages.register(app);
+  // commands.register(app);
+  // actions.register(app);
   // shortcuts.register(app);
   // views.register(app);
 };
